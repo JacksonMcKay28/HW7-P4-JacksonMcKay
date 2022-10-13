e1214e93b357e3d14416e2d8561a13c8dc61e9ae
JacksonMcKay28 <111894468+JacksonMcKay28@users.noreply.github.com>
2022-10-13 05:09:31 +0000

    added find and distance to fine location of min, DONE

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 //Title: HW7 Problem 4, License to Launch Kattis
 //Programmer: Jackson McKay
 //Course: CSCI 130-001 Dr. Mellenthin
+//    Cititions used on the pdf
 
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
-
 
 int main() {
   
@@ -21,4 +22,19 @@ int main() {
     cin >> sj[i];
   }
 
+  //int for minimum
+  int min;
+ //for loop to go through ints to find minimum
+  min = sj[0];
+  for (int i = 0; i < n; i++) {
+    if(sj[i] < min) {
+      min = sj[i];
+    }
+  }
+  //temporary cout for min
+  //cout << "This is the lowest val: " << min << endl;
+  
+  //int to find the location of min in sj array
+  int d = distance(sj, find(sj, sj + n, min));
+  cout << d;
 }
