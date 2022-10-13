d429c12109cf08c16a469b4dc7623debb710de30
JacksonMcKay28 <111894468+JacksonMcKay28@users.noreply.github.com>
2022-10-13 00:53:44 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+//Title: HW7 Problem 4, License to Launch Kattis
+//Programmer: Jackson McKay
+//Course: CSCI 130-001 Dr. Mellenthin
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  
+}
